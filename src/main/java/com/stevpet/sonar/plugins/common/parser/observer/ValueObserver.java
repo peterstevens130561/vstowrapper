dd7bb88aca13bd175b70016d4675c6b78b31009d
Peter Stevens <peter.stevens@bakerhughes.com>
2016-09-10 11:52:41 +0200

    ValueObserver - made generic. tests pass

--- src/main/java/com/stevpet/sonar/plugins/common/parser/observer/ValueObserver.java
+++ src/main/java/com/stevpet/sonar/plugins/common/parser/observer/ValueObserver.java
@@ -1,5 +1,5 @@
 package com.stevpet.sonar.plugins.common.parser.observer;
 
-public interface ValueObserver {
-    public void observe(String value);
+public interface ValueObserver extends ArgumentObserver<String>{
+
 }
