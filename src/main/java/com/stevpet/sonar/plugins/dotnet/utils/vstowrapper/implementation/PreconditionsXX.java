cd0573537d126b9e594d2b26b8b2d921433d5f76
Peter Stevens <peter.stevens@bakerhughes.com>
2016-10-27 10:16:12 +0200

    Missed

---
+++ src/main/java/com/stevpet/sonar/plugins/dotnet/utils/vstowrapper/implementation/PreconditionsXX.java
@@ -0,0 +1,22 @@
+package com.stevpet.sonar.plugins.dotnet.utils.vstowrapper.implementation;
+
+import java.io.File;
+
+public class PreconditionsXX {
+
+	public static void checkArgument(boolean b, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void checkNotNull(File workDir, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void checkState(boolean hasModules, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
